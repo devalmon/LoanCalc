a91dd5f92c87d046102c213f8afdd987cce7cbea
devalmon <phlood@yandex.ru>
2017-03-29 10:51:43 +0300

    facelift indents

--- LoanCalc/FirstSceneVC.swift
+++ LoanCalc/FirstSceneVC.swift
@@ -9,30 +9,31 @@
 import UIKit
 
 class FirstSceneVC: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
-    @IBAction func unwind(segue: UIStoryboardSegue) {}
-    
-
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+  @IBAction func unwind(segue: UIStoryboardSegue) {}
+  @IBAction func unwindFrom4VC(segue: UIStoryboardSegue) {}
+  
+  
 }
